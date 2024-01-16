5c49b7db854f28ac77fcc29e322e7a265119cedd
Ezequiel Lopez <ezequiel20139@gmail.com>
2024-01-16 12:52:58 -0300

    First try with docker and python inside

---
+++ pipeline.py
@@ -0,0 +1,11 @@
+import sys
+
+import pandas as pd
+
+params = sys.argv
+print(f"Start ETL with params = {params}..")
+
+df = pd.DataFrame()
+# Doing somethings with pandas
+
+print("ETL ends successfully.")
