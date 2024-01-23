da5222f53c0169dc37f7d7d8ae0d368201b2156e
Ezequiel Lopez <ezequiel20139@gmail.com>
2024-01-22 21:20:18 -0300

    Insert into postgres after download parquet

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,8 @@ COPY ./ /zoomcamp24
 ENV PYTHONPATH=${PYTHONPATH}:${PWD}
 ENV SOME_OTHER_ENV_VARIABLE=X
 
+RUN mkdir -p .docker_volumes/downloads
+
 RUN pip3 install --upgrade pip
 RUN pip3 install poetry
 RUN poetry config virtualenvs.create false
--- src/app/data/etl/main.py
+++ src/app/data/etl/main.py
@@ -6,11 +6,16 @@ import pandas as pd
 import requests
 import wget
 from dotenv import load_dotenv
+from sqlalchemy import create_engine
 
 parser = argparse.ArgumentParser()
 
-parser.add_argument("--date", help="Date for dataset. Format accepted 'yyyy-mm-dd'.")
-parser.add_argument("--taxi-color", help="Taxi color. Could be yellow or green.")
+parser.add_argument(
+    "--date", help="Date for dataset. Format accepted 'yyyy-mm-dd'.", required=True
+)
+parser.add_argument(
+    "--taxi-color", help="Taxi color. Could be yellow or green.", required=True
+)
 
 args = parser.parse_args()
 
@@ -42,9 +47,43 @@ if __name__ == "__main__":
         + f"{params.get('taxi_color')}_tripdata_{params.get('date')[:7]}.parquet"
     )
 
+    print("-" * 10 + "\nTARGETS:\n")
     print(f"TAXI URL: {TAXI_TARGET_URL}")
     print(f"ZONE URL: {ZONE_URL}")
+
+    print("-" * 10 + "\nDOWNLOADING DATA...\n")
     taxi_file = download_with_wget(TAXI_TARGET_URL)
     zone_file = download_with_requests(ZONE_URL)
+
+    print()
+    print("-" * 10 + "\nINSERTING DATA...\n")
+
+    # db_url = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@localhost:{os.getenv('POSTGRES_CONTAINER_PORT')}/{os.getenv('POSTGRES_DB')}"
+    db_url = f"postgresql://{os.getenv('POSTGRES_USER')}:{os.getenv('POSTGRES_PASSWORD')}@localhost:{os.getenv('POSTGRES_CONTAINER_PORT')}/{os.getenv('POSTGRES_DB')}"
+    engine = create_engine(db_url)
+
+    df_trips = pd.read_parquet(
+        f".docker_volumes/downloads/{params.get('taxi_color')}_taxi_{params.get('date')[:7]}.parquet",
+        "fastparquet",
+    )
+    # print(pd.io.sql.get_schema(df_trips, name="green_taxi_data", con=engine))
+    df_trips.to_sql(
+        name=f"{params.get('taxi_color')}_taxi_data",
+        con=engine,
+        if_exists="replace",
+        index=False,
+    )
+
+    df_zones = pd.read_parquet(
+        ".docker_volumes/downloads/zone.parquet",
+        "fastparquet",
+    )
+    # print(pd.io.sql.get_schema(df_zones, name="green_taxi_data", con=engine))
+    df_zones.to_sql(
+        name="zones",
+        con=engine,
+        if_exists="replace",
+        index=False,
+    )
 
     print("ETL ends successfully.")
